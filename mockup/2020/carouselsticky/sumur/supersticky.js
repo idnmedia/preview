e918a48881d9e45c84dde9604ad08e768ebbf894
idnmedia <faizal.ardiyadi@idntimes.com>
2021-12-13 11:55:10 +0700

    awd

---
+++ mockup/2020/carouselsticky/sumur/supersticky.js
@@ -0,0 +1,88 @@
+if(top!=self){
+    window.frameElement.style = '-webkit-transition: all .5s linear 0s; -moz-transition: all .5s linear 0s;-ms-transition: all .5s linear 0s;-o-transition: all .5s linear 0s;transition: all .5s linear 0s; height: 100px; width: 100%; position: relative; bottom: 0; left:0; right:0; margin:auto;';
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
+//var expandOverlay = function(){
+//    if(top!=self){
+//         window.frameElement.style = '-webkit-transition: all .5s linear 0s; -moz-transition: all .5s linear 0s;-ms-transition: all .5s linear 0s;-o-transition: all .5s linear 0s;transition: all .5s linear 0s; height: 100vh; width: 100vw; position: fixed; left:0; right: 0; margin: auto; top: 50%; transform: translateY(-50%);'; 
+//    };
+//    $("#bs02").css({"display" : "block"});
+//}
+//
+//var closeExpandOverlay = function(){
+//    if(top!=self){
+//         window.frameElement.style = '-webkit-transition: all .5s linear 0s; -moz-transition: all .5s linear 0s;-ms-transition: all .5s linear 0s;-o-transition: all .5s linear 0s;transition: all .5s linear 0s; height: 50px; width: 320px; position: fixed; left:0; right: 0; margin: auto; bottom: 0;'; 
+//    };
+//    $("#bs02").css({"display" : "none"});
+//}
+//
+//
+//
+//
+//
+//$('#bs01').click(function() {
+//    if(top!=self){
+//        window.frameElement.style = '-webkit-transition: all .5s linear 0s; -moz-transition: all .5s linear 0s;-ms-transition: all .5s linear 0s;-o-transition: all .5s linear 0s;transition: all .5s linear 0s; height: 100vh; width: 100vw; position: fixed; left:0; right: 0; margin: auto; top: 50%; transform: translateY(-50%);'; 
+//    }
+//     $("#bs02").css({"display" : "block"});
+//});
+//$('.closeBtn02').click(function() {
+//    if(top!=self){
+//        window.frameElement.style = '-webkit-transition: all .5s linear 0s; -moz-transition: all .5s linear 0s;-ms-transition: all .5s linear 0s;-o-transition: all .5s linear 0s;transition: all .5s linear 0s; height: 50px; width: 320px; position: fixed; left:0; right: 0; margin: auto; bottom: 0;'; 
+//    }
+//    $("#bs02").css({"display" : "none"});
+//});
+
+
+
+
+
+
+
+
+
+
+//$(document).ready(function(){
+//    setTimeout(function(){ 
+//        expandOverlay();
+//    }, 1000);  
+//     setTimeout(function(){ 
+//        closeExpandOverlay();
+//    }, 3000); 
+//});
+//
+//
+
+
+
+
