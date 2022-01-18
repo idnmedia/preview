87dc10fb8b7240e9a95234091f24c456d791c084
idnmedia <faizal.ardiyadi@idntimes.com>
2022-01-18 09:48:42 +0700

    segs

---
+++ m.popmama/carouselsticky/cussons/sumur/carouselsticky.js
@@ -0,0 +1,26 @@
+if(top!=self){
+    window.frameElement.style = '-webkit-transition: all .5s linear 0s; -moz-transition: all .5s linear 0s;-ms-transition: all .5s linear 0s;-o-transition: all .5s linear 0s;transition: all .5s linear 0s; height: 120px; width: 100%; position: relative; bottom: 0; left:0; right:0; margin:auto;';
+}
+
+
+// IDN Times
+const data = parent.document.getElementsByClassName('bottom-sticky-ads__wrapper')[0]
+data.style.maxWidth = "100%"
+data.style.maxHeight = "120px"
+
+
+
+function closecs() {
+    document.getElementById("cars").style.display = "none";
+    const data = parent.document.getElementsByClassName('bottom-sticky-ads__wrapper')[0]
+    data.style.maxHeight = "0px"
+    data.style.minHeight = "0px"
+}
+
+
+
+
+
+
+
+
